c36031fb5caa328a335461714336a3d401eccd2a
Facundo Jauregui <jaureguifacundo@gmail.com>
2014-10-03 23:00:48 -0300

    Updates & new object

--- automata.js
+++ automata.js
@@ -2,18 +2,48 @@
  * Formal Languajes Utils
  * @author facundoj
  */
-(function (w) {
-    w.Automata = w.Automata || {};
+(function (window) {
+    var Automata = Automata || {};
+
+    /**
+     * Validates and throws exception when not true
+     * @param assertion {Boolean} To check
+     * @param msg {String} Exception message
+     */
+    function assert (assertion, msg) {
+        if (!assertion) throw msg;
+    }
+
+    /**
+     * Context-sensitive grammar production rule
+     * @constructor
+     * @param left {String} left part of the rule (aAb)
+     * @param rigth {String} right part of the rule (aGb)
+     */
+    var TuringProdRule = function (left, right) {
+        assert(
+            typeof left === 'string' && typeof right === 'string',
+            "Invalid parameter"
+        );
+
+        if (left.length === 1) {
+
+        } else if (left.length === 2) {
+
+        } else if (left.length === 3) {
+
+        }
+    };
 
     /**
      * Capable to generate context-sensitive-languaje strings
      * @constructor
      */
-    w.Automata.Turing = function () {
+    Automata.Turing = function () {
         this.rules = [];
     };
 
-    w.Automata.Turing.prototype = {
+    Automata.Turing.prototype = {
         /**
          * Adds a production rule
          * @param left {String} left part of the rule (aAb)
@@ -32,4 +62,5 @@
         }
     };
 
+    window.Automata = Automata;
 })(this);
